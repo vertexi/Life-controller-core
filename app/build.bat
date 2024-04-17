a256a23066e78687ece98e4dd98b9daa630b97e1
Vertexi <778950965@qq.com>
2024-04-17 18:59:26 +0800

    build emscripten with one click.

--- app/build.bat
+++ app/build.bat
@@ -2,3 +2,4 @@ emcmake cmake . -B build_emscripten -DCMAKE_BUILD_TYPE=Release -G Ninja
 cmake --build build_emscripten
 
 python app\imgui_bundle\external\hello_imgui\hello_imgui\tools\emscripten\webserver_multithread_policy.py -p 8080
+cd .\build_emscripten\app\ llvm-dwp -e .\Life-controller-app.wasm -o .\Life-controller-app.wasm.dwp && cd ../../
