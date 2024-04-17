f1a2847ba29e0b1d86a50fde71898dfbb7036d27
Vertexi <778950965@qq.com>
2024-04-17 11:58:41 +0800

    Added doctest for improved code documentation and testing.

---
+++ app/doctest_header.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define DOCTEST_CONFIG_SUPER_FAST_ASSERTS
+#define DOCTEST_CONFIG_TREAT_CHAR_STAR_AS_STRING
+#include "doctest.h"
+
--- app/main.cpp
+++ app/main.cpp
@@ -2,6 +2,9 @@
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 #endif
 
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest_header.h"
+
 #include <cstdio>
 #include <cstring>
 #include <string>
@@ -803,8 +806,31 @@ std::string linesUnion(const std::string& input1, const std::string& input2)
 }
 #include "app_config.hpp"
 
-int main(int, char*[])
+int main(int argc, char** argv)
 {
+    doctest::Context context;
+
+    // !!! THIS IS JUST AN EXAMPLE SHOWING HOW DEFAULTS/OVERRIDES ARE SET !!!
+
+    // defaults
+    context.addFilter("test-case-exclude", "*math*"); // exclude test cases with "math" in their name
+    context.setOption("abort-after", 5);              // stop test execution after 5 failed assertions
+    context.setOption("order-by", "name");            // sort the test cases by their name
+
+    context.applyCommandLine(argc, argv);
+
+    // overrides
+    context.setOption("no-breaks", true);             // don't break in the debugger when assertions fail
+
+    int res = context.run(); // run
+
+    if(context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
+        return res;          // propagate the result of the tests
+
+    int client_stuff_return_code = 0;
+    // your program - if the testing framework is integrated in your production code
+    printf("hello world!\n");
+
     set_log_base_dir(LOG_BASE_DIR);
     std::string input1 = R"(a,b,c,d
 e,f,g,h
@@ -919,5 +945,5 @@ k,l,m,n
     runnerParams.imGuiWindowParams.showStatusBar = true;
     ImmApp::Run(runnerParams, addOnsParams);
 
-    return 0;
+    return res + client_stuff_return_code; // the result from doctest is propagated here as well
 }
